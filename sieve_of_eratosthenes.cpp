6582524a7e5c670cabef45ca9b350e1dca81c50e
neerajxrana <neerajrana2001@gmail.com>
2020-09-27 15:20:33 +0530

    added sieve_of_eratosthenes.cpp

---
+++ sieve_of_eratosthenes.cpp
@@ -0,0 +1,34 @@
+//Sieve of Eratosthenes
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> sieve_erato(int n){
+	vector<bool> num(n+1);
+	vector<int> primes;
+
+	fill(num.begin(), num.end(), true);
+	for(int i=2;i<=n;i++){
+		if(num[i]){
+			for(int p=i*i;p<=n;p+=i){
+				num[p] = false;
+			}
+		}
+	}
+
+	primes.push_back(2);
+	for(int i=3;i<=n;i+=2){
+		if(num[i]){
+			primes.push_back(i);
+		}
+	}
+
+	return primes;
+}
+
+int main(){
+	int n = 1000;
+	vector<int> primes = sieve_erato(n);
+	for(int a:primes) cout << a << " ";
+	return 0;
+}
