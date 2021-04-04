a439cefc090c6ac3ad457fe24002d798bf7e741f
neerajxrana <neerajrana2001@gmail.com>
2021-04-04 12:13:21 +0530

    added files to recursion

---
+++ recursion/all_index_of_number.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int all_index(int* arr, int size, int x, vector<int> &ans){
+	if(size == 0) return 0;
+	if
+}
+
+int main(){
+	int arr[] = {5,6,5,5,6,5};
+	int size = 6;
+	int x = 5;
+	vector<int> ans;
+	cout << all_index(arr, size, x, ans);
+}
--- recursion/last_index_of_number.cpp
+++ recursion/last_index_of_number.cpp
@@ -1,19 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int last_index(int* arr, int size, int x){
-	if(size == 0) return -1;
-	if(arr[size-1] == x) return size-1;
-	int arr2[size-1];
-	int ans = copy(*arr, *arr+size, *arr2);	
-	if(ans == -1) return -1;
-	else return ans - 1;
+int last_index(int* arr, int idx, int x){
+	if(idx < 0) return -1;
+	if(arr[idx] == x) return idx; 
+	return last_index(arr, idx-1, x);
 }
 
 int main(){
 	int arr[] = {4,5,5,4,4,7,4};
-	int size = 7;
-	int x = 4;
-	cout << last_index(arr, size, x);
+	int idx = 7;
+	int x = 5;
+	cout << last_index(arr, idx, x);
 	return 0;
 }
