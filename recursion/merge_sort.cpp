df6ca0247e87fe8a6df3ba4b0be76a5fbc7a6e22
neerajxrana <neerajrana2001@gmail.com>
2021-04-07 17:32:38 +0530

    added merge_sort to recursion

---
+++ recursion/merge_sort.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int merge(int arr[], int l, int m, int r){
+	int n = r - l;
+	int k = 0;
+	int i = l;
+	int j = m + 1;
+
+	while(i<=m && j<=r)
+}
+
+void mergesort(int arr[], int l, int r){
+	if(l<r){
+		int m = l + (r-1)/2;
+		mergesort(arr, l, m);
+		mergesort(arr, m+1, r);
+		merge(arr, l, m, r);
+	}
+}
+
+int main(){
+	int arr = {4,6,6,19,0,1};
+	int size = sizeof(arr)/sizeof(arr[0]);
+	mergesort(arr, 0, size-1);
+	for(int i:arr) cout << i << " ";
+	return 0;
+}
